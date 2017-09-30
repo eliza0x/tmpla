93bed095f068cd3145dff3ef2295fdfcfb7c5c51
eliza0x <me@eliza.link>
2017-09-30 18:53:01 +0900

    Update: Utilのドキュメントを書いた

--- src/Util.hs
+++ src/Util.hs
@@ -2,6 +2,16 @@ module Util
     ( genUUID
     ) where
 
+{- |
+Module      : Util
+Description : 便利ツールズ
+Copyright   : (c) eliza0x, 2017
+License     : MIT
+Maintainer  : me@eliza.link
+Stability   : experimental
+-}
+
+
 import qualified Data.UUID as U
 import qualified Data.UUID.V4 as U
 
